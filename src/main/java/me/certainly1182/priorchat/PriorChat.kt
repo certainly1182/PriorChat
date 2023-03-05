6057bb42bb58d813505a5cd35c1abeaff2dacaba
certainly1182 <fawcettjames63@gmail.com>
2023-03-05 20:38:59 +0000

    remove unnecessary TextComponent creation

--- src/main/java/me/certainly1182/priorchat/PriorChat.kt
+++ src/main/java/me/certainly1182/priorchat/PriorChat.kt
@@ -2,16 +2,17 @@ package me.certainly1182.priorchat
 
 import com.google.common.collect.EvictingQueue
 import io.papermc.paper.event.player.AsyncChatEvent
+import net.kyori.adventure.audience.Audience
 import net.kyori.adventure.text.Component
-import net.kyori.adventure.text.format.NamedTextColor
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.plugin.java.JavaPlugin
 import java.util.*
-import net.kyori.adventure.text.format.TextColor
 import org.bukkit.Bukkit
+import org.bukkit.event.entity.PlayerDeathEvent
 import org.bukkit.event.player.PlayerQuitEvent
+import org.bukkit.scheduler.BukkitRunnable
 
 class PriorChat : JavaPlugin(), Listener {
     private lateinit var messageCache: Queue<Component>
@@ -30,35 +31,39 @@ class PriorChat : JavaPlugin(), Listener {
     @EventHandler
     fun onPlayerJoin(event: PlayerJoinEvent) {
         val player = event.player
-        player.sendMessage("Chat History:")
         // send player history
         for (message in messageCache) {
             player.sendMessage(message)
         }
-        // log the event
-        messageCache.add(Component.text("[").color(NamedTextColor.GRAY)
-            .append(Component.text("+").color(TextColor.fromHexString("#55FFC6")))
-            .append(Component.text("] ").color(NamedTextColor.GRAY))
-            .append(player.displayName()))
+        runOnNextTick { messageCache.add(event.joinMessage()) }
     }
 
     @EventHandler
     fun onPlayerChat(event: AsyncChatEvent) {
         val player = event.player
         val message = event.message()
-        messageCache.add(Component.text()
-            .append(player.displayName())
-            .append(Component.text(": ").color(NamedTextColor.GRAY))
-            .append(message).build())
+        val renderer = event.renderer()
+        val rendered = renderer.render(player, player.displayName(), message, Audience.empty())
+        messageCache.add(rendered)
     }
 
     @EventHandler
     fun onPlayerLeave(event: PlayerQuitEvent) {
-        val player = event.player
-        // log the event
-        messageCache.add(Component.text("[").color(NamedTextColor.GRAY)
-            .append(Component.text("-").color(TextColor.fromHexString("#C655FF")))
-            .append(Component.text("] ").color(NamedTextColor.GRAY))
-            .append(player.displayName()))
+        runOnNextTick { messageCache.add(event.quitMessage()) }
+    }
+
+    @EventHandler
+    fun onPlayerDeath(event: PlayerDeathEvent) {
+        runOnNextTick { messageCache.add(event.deathMessage()) }
+    }
+
+    // Schedule an expression to run next tick
+    // allows other plugins to modify event message formats
+    private fun runOnNextTick(block: () -> Unit) {
+        object : BukkitRunnable() {
+            override fun run() {
+                block()
+            }
+        }.runTask(this)
     }
 }
