a543e0886937c19830c528d6a8bafb1a968d70bb
certainly1182 <fawcettjames63@gmail.com>
2023-03-07 22:58:33 +0000

    add config for which messages to enable

--- src/main/java/me/certainly1182/priorchat/PriorChat.kt
+++ src/main/java/me/certainly1182/priorchat/PriorChat.kt
@@ -16,7 +16,13 @@ import org.bukkit.scheduler.BukkitRunnable
 
 class PriorChat : JavaPlugin(), Listener {
     private lateinit var messageCache: Queue<Component>
-
+    enum class MessageType {
+        JOIN,
+        CHAT,
+        QUIT,
+        DEATH
+    }
+    private lateinit var enabledMessageTypes: Set<MessageType>
     override fun onEnable() {
         // Plugin startup logic
         saveDefaultConfig()
@@ -24,12 +30,23 @@ class PriorChat : JavaPlugin(), Listener {
         Bukkit.getLogger().info("Cache Size: $cacheSize")
         messageCache = EvictingQueue.create(cacheSize)
 
+        // Read enabled message types from config
+        val enabledMessageTypesConfig = config.getStringList("enabled_message_types")
+        enabledMessageTypes = enabledMessageTypesConfig.mapNotNull {
+            try {
+                MessageType.valueOf(it.uppercase())
+            } catch (ex: IllegalArgumentException) {
+                null
+            }
+        }.toSet()
+
         // Register event listener
         server.pluginManager.registerEvents(this, this)
     }
 
     @EventHandler
     fun onPlayerJoin(event: PlayerJoinEvent) {
+        if (!enabledMessageTypes.contains(MessageType.JOIN)) return
         val player = event.player
         // send player history
         for (message in messageCache) {
@@ -40,6 +57,7 @@ class PriorChat : JavaPlugin(), Listener {
 
     @EventHandler
     fun onPlayerChat(event: AsyncChatEvent) {
+        if (!enabledMessageTypes.contains(MessageType.CHAT)) return
         val player = event.player
         val message = event.message()
         val renderer = event.renderer()
@@ -49,11 +67,13 @@ class PriorChat : JavaPlugin(), Listener {
 
     @EventHandler
     fun onPlayerLeave(event: PlayerQuitEvent) {
+        if (!enabledMessageTypes.contains(MessageType.QUIT)) return
         runOnNextTick { messageCache.add(event.quitMessage()) }
     }
 
     @EventHandler
     fun onPlayerDeath(event: PlayerDeathEvent) {
+        if (!enabledMessageTypes.contains(MessageType.DEATH)) return
         runOnNextTick { messageCache.add(event.deathMessage()) }
     }
 
